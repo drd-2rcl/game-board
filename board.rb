03f3177bb9010003a2be2f42eb4ac325897322cf
Diogo <drd.rclrd@gmail.com>
2019-09-23 22:04:00 -0300

    a little refactoring

--- board.rb
+++ board.rb
@@ -1,20 +1,25 @@
 class Board
+  WIDTH = 4
+  HEIGHT = WIDTH
+
   attr_accessor :grid
   
   def initialize
-    @grid = [
-      [[],[],[]],
-      [[],[],[]],
-      [[],[],[]]
-    ]
+    @grid = Array.new(HEIGHT) { Array.new(WIDTH,:" ")}
+  end
+
+  def print_grid
+    @grid.each do |row|
+      puts
+      row.each do |cell|
+        print "[#{cell}]"
+      end
+    end
+    puts
   end
 
 end
 
-p Board.new.grid
-
-puts "
-[][][]
-[][][]
-[][][]
-"
+b = Board.new
+b.print_grid
+b.print_grid
